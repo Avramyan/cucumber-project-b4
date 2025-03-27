3405c4c2ca838f9c657c8c1b2122dea4cae99eac
avramyan <avramovartjom@gmail.com>
2025-03-27 09:02:54 -0400

    cucumber intro

--- src/test/java/io/loop/runner/CukesRunner.java
+++ src/test/java/io/loop/runner/CukesRunner.java
@@ -7,9 +7,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber-reports/cucumber.json", "html:target/cucumber-reports/index.html"},
+        plugin = {"json:target/json-reports/cucumber.json", "html:target/cucumber-reports/index.html"},
         features = "src/test/resources/features",
-        glue = "io.loop.test.stepdefinitions",
+        glue = "io.loop.test.step_definitions",
         dryRun = true,
         tags = "@AATest1 or @AATest2"
 )
--- src/test/java/io/loop/step_definitions/LoginStepDefs.java
+++ src/test/java/io/loop/step_definitions/LoginStepDefs.java
@@ -17,18 +17,17 @@ public class LoginStepDefs {
     }
     @When("user enters password for client")
     public void user_enters_password_for_client() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("user click login button")
     public void user_click_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("user should be able to see the home for client")
     public void user_should_be_able_to_see_the_home_for_client() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
+
+
 
 }
